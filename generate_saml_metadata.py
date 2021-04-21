bc4da2c016b22d6e8b93ecb2ed47d73c5a603cac
Dolsy Smith <dsmith@gworcid-dev.wrlc.org>
2021-04-21 11:37:07 -0400

    Added utility to generate standalone XML SP metadata

---
+++ generate_saml_metadata.py
@@ -0,0 +1,22 @@
+import os
+from onelogin.saml2.settings import OneLogin_Saml2_Settings
+
+saml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'saml')
+
+def get_metadata():
+    settings = OneLogin_Saml2_Settings(custom_base_path=saml_path)
+    metadata = settings.get_sp_metadata()
+    errors = settings.validate_metadata(metadata)
+    if len(errors) == 0:
+        return metadata
+    else:
+        print(errors)
+
+def save_metadata():
+    metadata = get_metadata()
+    if metadata:
+        with open(saml_path + '/orcid-integration-metadata.xml', 'wb') as f:
+            f.write(metadata)
+
+if __name__ == '__main__':
+    save_metadata()
