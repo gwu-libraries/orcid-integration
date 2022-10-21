2e8a6a0c5b7dbab044850caa3ac93567a6ba74b7
Dolsy Smith <dsmith@gwu.edu>
2022-10-21 15:44:06 -0400

    Updating examples

--- example.config.py
+++ example.config.py
@@ -1,6 +1,9 @@
 SECRET_KEY = b''
 CLIENT_ID = ''
 CLIENT_SECRET = ''
+SERVER_NAME = ''
 SLO_REDIRECT = ''
 ORCID_SUCCESS_URL = ''
 ORCID_FAILURE_URL = ''
+# Set to true to prepopulate ORCID's registration for with values from the SAML IdP
+PREFILL_REGISTRATION = False
