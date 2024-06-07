974dcf759ec6d91f62a880447fc983478b7c9140
Dolsy Smith <dsmith@gwu.edu>
2024-06-07 13:54:30 -0400

    Added ORCiD schema for works

--- orcid.py
+++ orcid.py
@@ -1,8 +1,25 @@
 from dataclasses import dataclass
 from typing import Optional
 import re
+from jinja2 import Environment, PackageLoader
 
-LYTERATI_TO_ORCID_MAPPING = {column: 'publisher' for column in ['publisher', 'publication_venue', 'conference', 'event', 'distributor']}
+def finalize(value):
+    '''
+    Ensures that Jinja passes an empty string and "None" where None is set in the Python object
+    '''
+    return value if value is not None else ''
+
+L2O_MAPPING = {'column': {column: 'publisher' for column in ['publisher',
+                                                                            'publication_venue', 
+                                                                            'conference', 
+                                                                            'event', 
+                                                                            'distributor']},
+               'filename': {}}
+
+ENV = Environment(
+    loader=PackageLoader(package_name='orcid', package_path='schemas'),
+    finalize=finalize
+)
 
 @dataclass
 class ORCiDWork:
@@ -11,12 +28,14 @@ class ORCiDWork:
     '''
     title: str                              # work title
     container: str                          # publisher, journal title, etc.
-    authors: list[dict[str, str]]           # author names, possibly with ORCiD and sequence indicator
+    contributors: list[dict[str, str]]      # author names, possibly with ORCiD and sequence indicator
     work_type: str                          # ORCiD work type
     pub_year: str                           # publication year
     pub_month: Optional[str] = None         # possible publication month
     pub_day: Optional[str] = None           # possible day of publication
     doi: Optional[str] = None               # possible DOI
+
+    template = ENV.get_template('work-full-3.0.json') # template for works
 
     @staticmethod
     def split_authors(authors_str: str) -> list[str]:
@@ -26,4 +45,4 @@ class ORCiDWork:
         return [author.strip() for author in authors if author.strip()]
     
     def create_json(self):
-        pass
+       return ORCiDWork.template.render(work=self) 
--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -1,6 +1,7 @@
 import pytest
-from lyterati_utils import Lyterati
-from orcid import ORCiDWork
+from orcid.lyterati_utils import Lyterati
+from orcid.orcid import ORCiDWork
+import json
 
 
 @pytest.fixture
@@ -15,6 +16,20 @@ def file_path():
 def author_names():
     return ['Beatrix Potter', 'Frank Chateaubriand']
 
+@pytest.fixture
+def contributors():
+    return [{'name': 'Beatrix Potter',
+             'orcid': '0000-0000-0000-0000'},
+             {'name': 'Kenneth Grahame',
+              'orcid': '0000-0000-0000-0001'}]
+
+@pytest.fixture
+def work():
+    return {'title': 'A fascinating monograph',
+            'container': 'Oxfurred University Press',
+            'work_type': 'book',
+            'pub_year': '2000'}
+
 class TestLyterati:
 
     def test_load_files(self, user_id, file_path):
@@ -24,7 +39,7 @@ class TestLyterati:
         assert lyterati.user_data[1]["title"] == 'Fascinating problems in fascinating literature'
         assert { k for record in lyterati.user_data for k in record.keys() } == {'gw_id', 'title', 'publisher', 'authors', 'start_year','file_name'}
 
-class TestORCiD:
+class TestORCiDWord:
 
     def test_split_authors(self, author_names):
         author_str_and = ' and '.join(author_names)
@@ -33,3 +48,8 @@ class TestORCiD:
         for author_str in [author_str_and, author_str_with, author_str_comma]:
             assert ORCiDWork.split_authors(author_str) == author_names
 
+    def test_create_work(self, work, contributors):
+        work.update({'contributors': contributors})
+        orcid_work = ORCiDWork(**work)
+        json_work = orcid_work.create_json()
+        assert json.loads(json_work) # tests for valid JSON
